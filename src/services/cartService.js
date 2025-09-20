dbb5c0bac868a1cd6df33a3615c8264ebdf37121
Amukthakotha <163736477+Amukthakotha@users.noreply.github.com>
2025-09-20 09:52:47 +0530

    Update cartService.js

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://ec2-34-201-149-205.compute-1.amazonaws.com:8081/api/cart";
+const API_URL = "http://ec2-54-152-253-133.compute-1.amazonaws.com:8081/api/cart";
 
 const API_BASE = "http://ec2-54-152-253-133.compute-1.amazonaws.com:8081/api/cart";
 
